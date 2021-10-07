3c44f29670d89b61022aa5c3903454477f5f42d5
Fernando dos Santos <nando4803@gmail.com>
2021-10-07 16:44:11 +0100

    feat: add menu resources

---
+++ src/resources/data.tsx
@@ -0,0 +1,9 @@
+import { FiBookOpen, FiMusic, FiShare } from 'react-icons/fi'
+
+const data = [
+  { name: 'Read', url: '/', icon: <FiBookOpen />, id: 'read' },
+  { name: 'Listen', url: '/', icon: <FiMusic />, id: 'listen' },
+  { name: 'Share', url: '/', icon: <FiShare />, id: 'share' }
+]
+
+export default data
---
+++ src/resources/index.tsx
@@ -0,0 +1,9 @@
+import { FiHome, FiBook, FiUser } from 'react-icons/fi'
+
+const menuData = [
+  { name: 'Home', url: '/', icon: <FiHome />, id: 'home' },
+  { name: 'Library', url: '/library', icon: <FiBook />, id: 'library' },
+  { name: 'Profile', url: '/', icon: <FiUser />, id: 'profile' }
+]
+
+export default menuData
