499d785a39839f3bee304a4ec2f0b70a7fc82e76
Fernando dos Santos <nando4803@gmail.com>
2021-10-07 16:51:20 +0100

    feat: add labrary page

---
+++ src/pages/Library/index.tsx
@@ -0,0 +1,13 @@
+import { Books, Container, Search, Menu } from 'components'
+
+const FindBook = () => {
+  return (
+    <Container>
+      <Search />
+      <Books />
+      <Menu />
+    </Container>
+  )
+}
+
+export default FindBook
